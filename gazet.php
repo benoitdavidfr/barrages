34e008772777675644bd7ab6628ec21db1eaec61
Benoit DAVID <benoit.davi@free.fr>
2020-02-01 18:34:50 +0000

    affichage de plusieurs couches dans la carte

--- gazet.php
+++ gazet.php
@@ -1,30 +1,36 @@
 <?php
 /*PhpDoc:
 name: gazet.php
-title: gazet.php - liste des barrages
+title: gazet.php - liste des barrages correspondant à l'un des fichiers CSV
 doc: |
-  Si pas de paramètre affiche la liste des barrages avec des liens vers:
+  Le paramètre file donne le nom du fichier CSV dans data
+  Si pas d'autre paramètre affiche la liste des barrages du fichier avec des liens vers:
     - le détail du barrage
     - le graphique associé au barrage
     - la carte zoomée sur le barrage
 journal: |
 */
 
-if (!($file = fopen(__DIR__."/data/retenues-20200121-Occitanie.csv",'r')))
-  die("Erreur ouverture du fichier retenues-20200121-Occitanie.csv");
+if (!isset($_GET['file'])) {
+  die("Erreur paramètre file absent");
+}
+if (!($file = fopen(__DIR__."/data/$_GET[file]",'r'))) {
+  die("Erreur ouverture du fichier $_GET[file]");
+}
+
 $header = fgetcsv($file, 1024, ';', '"');
 
 if (!isset($_GET['num'])) {
   echo "<!DOCTYPE HTML><html>";
-  echo "<head><title>liste barrages</title><meta charset=\"UTF-8\"></head><body>";
-  echo "<h2>Liste des barrages</h2>\n";
+  echo "<head><title>liste $_GET[file]</title><meta charset=\"UTF-8\"></head><body>";
+  echo "<h2>Liste des barrages $_GET[file]</h2>\n";
   echo "<table border=1>\n";
   while ($record = fgetcsv($file, 1024, ';', '"')) {
     foreach ($header as $i => $k)
       $rec[$k] = $record[$i];
     $lon = str_replace(',','.',$rec['Lon']);
     $lat = str_replace(',','.',$rec['Lat']);
-    echo "<tr><td><a href='?num=$rec[Code]'>$rec[Nom]</a></td>",
+    echo "<tr><td><a href='?file=$_GET[file]&amp;num=$rec[Code]'>$rec[Nom]</a></td>",
       "<td><a href='chart.php?num=$rec[Code]'>graphique</a></td>",
       "<td><a href='map.php?lon=$lon&amp;lat=$lat&amp;level=15'>carte</a></td>",
       "</tr>\n";
--- index.php
+++ index.php
@@ -8,18 +8,28 @@ title: index.php - page d'accueil
 <head><title>POC barrages</title><meta charset="UTF-8"></head>
 <body>
 <h2>POC Barrages</h2>
-POC d'estimation du volume d'eau stocké dans les barrages - Pascal Kosuth - CGEDD.<br>
+Version du 1/2/2020.</p>
+
+POC d'estimation par analyse d'images satellitaires du volume d'eau stocké dans les barrages
+ - Pascal Kosuth - CGEDD.<br>
 Première maquette affichant les graphiques associés à chacun des barrages.<br>
-Dans cette première version, seuls les principaux barrages d'Occitanie sont traités
-et seul le graphique du réservoir de l'Astarac est disponible.
+Dans cette seconde version,  seul le graphique du réservoir de l'Astarac est disponible
+et 3 listes de barrages sont affichées :<ul>
+<li>les barrages de plus de 15 m France entière</li>
+<li>les barrages de plus de 15 m d'Occitanie et de Nelle Aquitaine</li>
+<li>les barrages utilisés pour validation de la méthode d'estimation</li>
+</ul>
 <h2>Menu</h2>
 <ul>
   <li><a href='map.php'>carte des barrages</a></li>
-  <li><a href='gazet.php'>liste des barrages</a></li>
+  <li><a href='gazet.php?file=Barrages_15m_France_20200131.csv'>liste des barrages 15m France entière</a></li>
+  <li><a href='gazet.php?file=Barrages_15m_Occitanie_NlAq_20200131.csv'>
+    liste des barrages 15m Occitanie et Nelle Aquitaine</a></li>
+  <li><a href='gazet.php?file=retenues-20200121-Occitanie.csv'>liste des barrages pour validation</a></li>
 </ul>
 <h3>Divers</h3>
 <ul>
-  <li><a href='geojson.php'>Test GeoJSON des barrages</a></li>
+  <li><a href='geojson.php?file=Barrages_15m_France_20200131.csv'>Test GeoJSON des barrages France entière</a></li>
   <li><a href='https://github.com/benoitdavidfr/barrages' target='_blank'>Github du code source</a></li>
   <li><a href='https://www.fusioncharts.com/charts#fusioncharts'>Bibliothèque fusioncharts</a></li>
 <?php
